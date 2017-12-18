533349b76bcf6592d034aff08214b173384616fd
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-12-17 20:35:18 -0500

    stashing aside changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,152 @@
 $(document).ready(function(){
 
+
+let intervalId;
+let clockRunning = false;
+
+const stopwatch = {
+
+  time: 120,
+
+  reset: function() {
+
+    stopwatch.time = 120;
+    $("#display").text("2:00");
+
+  },
+
+  start: function() {
+
+    if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+    }
+  },
+
+  stop: function() {
+
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+
+  count: function() {
+
+    stopwatch.time--;
+    const converted = stopwatch.timeConverter(stopwatch.time);
+    // console.log(converted);
+
+    $("#display").text(converted);
+
+    if(stopwatch.time == 0){
+    	stopwatch.stop();   	
+    }
+    return clockRunning;
+  },
+
+  timeConverter: function(t) {
+
+    let minutes = Math.floor(t / 60);
+    let seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+};
+	
+
+
 var triviaGame = {
 
 	correctCount : 0,
 	incorrectCount : 0,
+	unansweredCount : questionBank.length,
 
+	checkAnswers : function(selectedAns, correctAns){
+		if(selectedAns === correctAns){
+			this.correctCount++;
+		}else{
+			this.incorrectCount++;
+		}
+
+		this.unansweredCount = questionBank.length - (this.correctCount + this.incorrectCount);
+
+		console.log("correct=", this.correctCount, "incorrect=", this.incorrectCount, "unanswered=", this.unansweredCount);
+	},
+
+	getResults : function(){
+		return "<h4> Correct Answers : " + this.correctCount + "</h4>" + 
+		"<h4> Incorrect Answers : " + this.incorrectCount + "</h4>" + 
+		"<h4> Unanswered Questions : " + this.unansweredCount + "</h4>";
+	},
 
 	init : function(){
 
-		for(var index=0; index< questionBank.length; index++){
+	for(var index = 0; index < questionBank.length; index++){
 
-			let qnDivSection = $('<div>');
-			qnDivSection.attr("class", "col-md-12 div-qa");
-			qnDivSection.attr("data-id", questionBank[index].id);
+		let qnDivSection = $('<div>');
+		qnDivSection.attr("class", "col-md-12 div-qa");
+		qnDivSection.attr("data-id", questionBank[index].id);
 
-			let qnaInformation =
+		let qnaInformation =
 
-			'<h4 class="qn">' + questionBank[index].qn + '</h4><p>' +
-			'<span class="ans">' + questionBank[index].ans[0] + '</span>' + 
-			'<span class="ans">' + questionBank[index].ans[1] + '</span>'+ 
-			'<span class="ans">' + questionBank[index].ans[2] + '</span>' + 
-			'<span class="ans">' + questionBank[index].ans[3] + '</span></p>';
+		'<h4 class="qn">' + questionBank[index].qn + '</h4>';
 
-			qnDivSection.html(qnaInformation);
-
-			$(".row-main").append(qnDivSection);
-
+		for (var i = 0; i < 4; i++)
+		{
+			qnaInformation += "<span class='ans'><input type='radio' name='ans-radio' data-aid=" + i + ">&nbsp;&nbsp;&nbsp;" +  questionBank[index].ans[i] + "</span>";
 		}
+
+		qnDivSection.html(qnaInformation);
+		$(".row-main").append(qnDivSection);
 	}
-}
+},
 
-$('.data-id').on('click',function(){
-	console.log($(this).data());
-})
+	reset: function(){
+		this.correctCount = 0;
+		this.incorrectCount = 0;
+		this.unansweredCount = questionBank.length;
+
+	}
+};
 
 
-function checkAnswer(qn, ans){
+// if(!stopwatch.count()){
+// 	$("#resultsModal").modal('show');
+// 	$('.results-body').html(triviaGame.getResults());
 
-}
+// }
+
+
+$('.submitA').on('click', function(){
+	stopwatch.stop();
+	let results = triviaGame.getResults();
+	$('.results-body').html(results);
+});
+
+$('#wrapper').on( 'click', '.ans', function() {
+
+	$thisSpan = $(this);  // (to learn - event delegataion)
+	
+	let selectedAns = $thisSpan.children().data('aid');
+	let questionId = $thisSpan.parent().data('id');
+	let correctAns = questionBank[questionId].ansIndex;
+
+	triviaGame.checkAnswers(selectedAns, correctAns);
+
+});
+
 
 triviaGame.init();
+stopwatch.start();
 
 });
